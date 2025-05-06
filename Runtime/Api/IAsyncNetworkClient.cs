0f79fecdfba4be6ff6e9f1e562210bd6c5138d18
Moritz Wundke <b.thax.dcg@gmail.com>
2025-05-06 02:15:28 +0200

    Adjusting IAsyncNetworkClient interface

--- Runtime/Api/IAsyncNetworkClient.cs
+++ Runtime/Api/IAsyncNetworkClient.cs
@@ -7,6 +7,7 @@ namespace AsyncNetClient
     public interface IAsyncNetworkClient
     {
         ISerializer Serializer { get; }
+        void AddDecorator(IAsyncNetDecorator decorator);
         Task<ResponseContext> SendAsync(HttpMethod method, string path, object request = null, CancellationToken cancellationToken = default);
     }
 }
