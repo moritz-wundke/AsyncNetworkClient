746e1ac0d9744b5959b148dfbe1e5443c15aa02f
Moritz Wundke <b.thax.dcg@gmail.com>
2025-05-07 22:25:33 +0200

    Simplify serialization settings

--- Runtime/Serialization/NewtonsoftSerializer.cs
+++ Runtime/Serialization/NewtonsoftSerializer.cs
@@ -9,29 +9,28 @@ namespace AsyncNetClient.Serialization
     public class NewtonsoftSerializer : ISerializer
     {
         public string ContentType => "application/json";
-        private readonly JsonSerializer _serializer;
+        private readonly JsonSerializerSettings _serializerSettings;
 
         public NewtonsoftSerializer(JsonSerializerSettings settings = null)
-            : this(JsonSerializer.Create(settings)) {}
+        {
+            _serializerSettings = settings ?? new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.Auto
+            };
+        }
 
-        private NewtonsoftSerializer(JsonSerializer serializer)
-            => _serializer = serializer;
 
         public string SerializeObject<T>(T value)
         {
-            var builder = new StringBuilder(256);
-            using var writer = new StringWriter(builder, CultureInfo.InvariantCulture);
-            using var jsonWriter = new JsonTextWriter(writer);
-            jsonWriter.Formatting = _serializer.Formatting;
-            _serializer.Serialize(jsonWriter, value, typeof(T));
-
-            return writer.ToString();
+            return JsonConvert.SerializeObject(value, _serializerSettings);
         }
 
         public T DeserializeObject<T>(string value)
         {
-            using var reader = new JsonTextReader(new StringReader(value));
-            return (T)_serializer.Deserialize(reader, typeof(T));
+            return JsonConvert.DeserializeObject<T>(value, _serializerSettings);
         }
     }
 }
